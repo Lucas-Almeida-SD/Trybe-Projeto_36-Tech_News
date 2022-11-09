a4080d6b87b2ad8a9365b06e9597c0619c19e5c7
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-09 11:43:16 -0300

    feat: Requisito 12 e 13: Crie a função analyzer_menu e implementa as funcionalidades do menu
    permite ao usuário escolher uma das opções do menu e executa uma ação de acordo com a opção escolhida

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,48 @@
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_tag,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+import sys
+
+number_options = {
+    "0": lambda x: get_tech_news(
+        int(input("Digite quantas notícias serão buscadas:"))
+    ),
+    "1": lambda x: search_by_title(input("Digite o título:")),
+    "2": lambda x: search_by_date(
+        input("Digite a data no formato aaaa-mm-dd:")
+    ),
+    "3": lambda x: search_by_tag(input("Digite a tag:")),
+    "4": lambda x: search_by_category(input("Digite a categoria:")),
+    "5": lambda x: top_5_news(),
+    "6": lambda x: top_5_categories(),
+    "7": lambda x: "Encerrando script\n",
+}
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    option = input(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por tag;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair."
+    )
+
+    try:
+        if option in number_options:
+            result = number_options[option](None)
+            print(result)
+        else:
+            sys.stderr.write("Opção inválida\n")
+    except Exception as err:
+        sys.stderr.write(err)
