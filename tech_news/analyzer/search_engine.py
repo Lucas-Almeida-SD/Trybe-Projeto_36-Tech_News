50406eb04a38f0a0cf0614749a35ee54ce097db7
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-09 01:25:48 -0300

    feat: Requisito 7: Crie a função search_by_date
    busca as news através da data e retorna seus titulos e  URLs em um tupla

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import search_news
 import re
+from datetime import datetime
 
 
 # Requisito 6
@@ -14,7 +15,20 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    splited_date = date.split('-')
+    year = int(splited_date[0])
+    month = int(splited_date[1])
+    day = int(splited_date[2])
+
+    try:
+        datetime(year, month, day)
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    news_list = search_news({"timestamp": '/'.join(splited_date[::-1])})
+    news_tuple = [(news["title"], news["url"]) for news in news_list]
+
+    return news_tuple
 
 
 # Requisito 8
